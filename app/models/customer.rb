b037bc1406c875207278fd074e516e59fa1cb4eb
野田健成 <kensei1016ab@gmail.com>
2020-02-10 12:57:29 +0900

    会員編集画面のバリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,16 @@ class Customer < ApplicationRecord
   has_many :deliveries, dependent: :destroy
 
   enum admittion_status: { withdraw: false, validity: true}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :kana_last_name
+    validates :kana_first_name
+    validates :zip_code
+    validates :address
+    validates :tel
+    validates :email, format: {with: VALID_EMAIL_REGEX}
+  end
 end
