38610a090dbce5290e356cec9512ae891b662735
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-10 03:09:26 +0000

    [Create]product-page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,14 +9,34 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product.id)
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :name, :image, :description, :price, :sales_status)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ApplicationRecord
 	belongs_to :genre
 	has_many :cart_items, dependent: :destroy
 	has_many :ordered_products,dependent: :destroy
+
+  attachment :image
+
+  validates :image, presence: true
+  validates :name, presence: true, length: {minimum: 1, maximum:15}
+  validates :description, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
+  validates :sales_status, inclusion: {in: [true, false]}
 end
