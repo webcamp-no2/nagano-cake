27d37cadb7f7611fc9a42e5ced2159bc9614e8c2
野田健成 <kensei1016ab@gmail.com>
2020-02-10 12:12:57 +0900

    ・会員編集画面完了 ・一覧、詳細で会員ステータスが反映されていない件修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,5 +12,17 @@ class Admin::CustomersController < Admin::BaseController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id), notice: '会員情報を更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :zip_code, :address, :tel, :email, :admittion_status)
   end
 end
