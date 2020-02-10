c7d55107c57ff6dc50a226f30f85a9b84b3e0135
mika0616 <nokolight0616@gmail.com>
2020-02-10 14:40:33 +0900

    [update]指摘項目を編集

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,6 @@ class Genre < ApplicationRecord
 
 	# バリデーション
 	validates :name, presence: true
-	validates :active_status, presence: true
 
 	# 有効無効ステータス
 	enum active_status: {"無効": false, "有効": true}
