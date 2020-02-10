87b97fe4a8f0cea93bd1221501c86efe835f98f4
mika0616 <nokolight0616@gmail.com>
2020-02-10 12:40:56 +0900

    [create]feature/create-admin-genre-edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,7 @@ class Admin::GenresController < Admin::BaseController
   def create
   	@genre = Genre.new(genre_params)
   	if @genre.save
-  		flash[:notice] = "You have creatad book successfully."
+  		flash[:notice] = "You have creatad new genre successfully."
   		redirect_to admin_genres_path
   	else
   		@genres = Genre.all
@@ -16,9 +16,18 @@ class Admin::GenresController < Admin::BaseController
   end
 
   def edit
+  		@genre = Genre.find(params[:id])
   end
 
   def update
+		@genre = Genre.find(params[:id])
+		if @genre.update(genre_params)
+			flash[:notice] = "You have edited genre successfully."
+			@genre = Genre.find(params[:id])
+			redirect_to admin_genres_path
+		else
+			render action: :edit
+		end
   end
 
 private
