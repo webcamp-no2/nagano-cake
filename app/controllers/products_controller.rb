282e997c67c51de6fff53c35fdcca89ecc2f0faa
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-11 07:58:55 +0000

    [create]product-function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.page(params[:page]).per(6).reverse_order
+    @genres = Genre.all
   end
 
   def show
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new(product_id: @product.id)
+    @genres = Genre.all
   end
 end
