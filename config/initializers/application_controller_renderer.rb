39524ac1be16e017e6fa7f38c0e81f48191e139e
mika0616 <nokolight0616@gmail.com>
2020-02-11 13:11:27 +0900

    リファイルシークレットキー追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0d06154d7a358ffb9d5d9ee3ec8ba6f0cdeea9ed5ffc1d5d4ba349208ef0253c8a719205ba07cbae5631046cdc78d03d7fbb059042a893efd2bbd59d3b1c1a2f'
