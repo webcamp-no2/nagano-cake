346b6ed3dd9c0ee9cb8a8d58e43b96fb4f712b4f
kensei1016 <kensei1016ab@gmail.com>
2020-02-11 23:49:48 +0900

    作業途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :address, :zip_code, :tel])
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :address, :zip_code, :tel])
-      # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :address, :zip_code, :tel])
     end
 end
