8c2d4114720bd00f541bd5f5516003e17fa9c5fa
mika0616 <nokolight0616@gmail.com>
2020-02-13 18:48:27 +0900

    [updat]create,updateアクションを追加

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,5 +1,19 @@
 class DeliveriesController < ApplicationController
+  def index
+    @delivery_address = Delivery.new
+    @customer = Customer.find(current_customer.id)
+  end
+
   def create
+    @delivery_address = Delivery.new(delivery_params)
+    @delivery_address.customer_id = current_customer.id
+    if @delivery_address.save
+      flash[:notice] = "配送先を登録しました"
+      redirect_to deliveries_path
+    else
+      @customer = Customer.find(current_customer.id)
+      render action: :index
+    end
   end
 
   def edit
@@ -7,13 +21,20 @@ class DeliveriesController < ApplicationController
   end
 
   def update
-  end
-
-  def index
-    @delivery_address = Delivery.new
-    @customer = Customer.find(current_customer.id)
+    @delivery_address = Delivery.find(params[:id])
+    if @delivery_address.update(delivery_params)
+      flash[:notice] = "配送先を編集しました。"
+      redirect_to deliveries_path
+    else
+      render action: :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+  def delivery_params
+    params.require(:delivery).permit(:name, :zip_code, :address)
   end
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,8 @@
 class Delivery < ApplicationRecord
 	belongs_to :customer
+
+	# バリデーション
+	validates :zip_code, presence: true
+	validates :address, presence: true
+	validates :name, presence: true
 end
