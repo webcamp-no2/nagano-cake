2b805971ba009997bebdc724d6e4f5ca8020ab9a
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-16 03:12:00 +0000

    genre検索機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page]).per(18).reverse_order
+    if params[:genre_id].present?
+      @products_of_genre = Product.where(genre_id: params[:genre_id])
+      .page(params[:page]).per(6).reverse_order
+    else
+      @products_all = Product.page(params[:page]).per(18).reverse_order
+    end
     @genres = Genre.all
   end
 
@@ -9,4 +14,5 @@ class ProductsController < ApplicationController
     @cart_item = CartItem.new(product_id: @product.id)
     @genres = Genre.all
   end
+
 end
