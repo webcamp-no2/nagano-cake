d23d45a24c9b855c6018e8fcc96031b0f719d229
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-02-19 08:15:27 +0000

    productのレイアウト変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,21 +2,21 @@ class ProductsController < ApplicationController
   def index
     if params[:genre_id].present?
       @products = Product.where(genre_id: params[:genre_id])
-      .page(params[:page]).per(18).reverse_order
+      .page(params[:page]).per(16).reverse_order
 
       @product_genre = @products.first.genre if @products.count > 0
     else
       # ジャンルが無効になっている商品は一覧に表示しない
       @products = Product
         .joins(:genre).where(genres: {active_status: "有効"})
-        .page(params[:page]).per(18).reverse_order
+        .page(params[:page]).per(16).reverse_order
     end
     @genres = Genre.all
   end
 
   def search
     # application controllerで生成した@qを利用して検索する
-    @q_products = @q.result.page(params[:page]).per(18).reverse_order
+    @q_products = @q.result.page(params[:page]).per(16).reverse_order
     @genres = Genre.all
 
     render :index
